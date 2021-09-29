a9790afe05de7e0e034e583a1f6f2fb0180838a9
Neiroh <raulito72001@gmail.com>
2021-09-29 09:42:17 +0200

    Corrección tanda3

--- Ejercicio3-7/index.php
+++ Ejercicio3-7/index.php
@@ -12,9 +12,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         
         <form action="media.php" method="post">
             
-            Nota 1: <input type="number" name="nota1"><br><br>
-            Nota 2: <input type="number" name="nota2"><br><br>
-            Nota 3: <input type="number" name="nota3"><br><br>
+            Nota 1: <input type="number" step="any" name="nota1"><br><br>
+            Nota 2: <input type="number" step="any" name="nota2"><br><br>
+            Nota 3: <input type="number" step="any" name="nota3"><br><br>
             
             <input type="submit" value="Enviar">
             
