07c4da6718575fd4a5b22575c705bdc03f0332f2
Neiroh <raulito72001@gmail.com>
2021-10-05 09:48:44 +0200

    me cago en la familia del creador de php

--- Ejercicio4-7/index.php
+++ Ejercicio4-7/index.php
@@ -12,28 +12,63 @@
 
     $num = $_POST['num'];
     $deseado = $_POST['deseado'];
-    $oportunidades = 4;
 
-    if(isset($num)){
-        if($oportunidades > 0){
-            if($num == $deseado){
-                echo('Enhorabuena fiera, has encontrado el número, enga a dormir');
+    if(!isset($oportunidades)){
+        $oportunidades = 4;
+    }else{
+        if(isset($num)){
+
+            if($oportunidades > 0){
+
+                if($num == $deseado){
+
+                    echo 'Has acertado, a mimir crack';
+
+                }else{
+
+                    echo 'Has fallado payaso';
+                    $oportunidades--;
+                    echo 'Te quedan' , $oportunidades , ' oportunidades';
+
+                }
+
             }else{
-                echo('Has fallado payaso<br><br>');
+
+                echo 'Te has quedado sin oportunidades';
+                
             }
-            $oportunidades--;
-            echo("Te quedan $oportunidades oportunidades<br><br>");
-            //yo
-        }else{
-            echo ('Te has quedado sin oportunidades');
+
         }
     }
+
+    /*$num = $_POST['num'];
+    $deseado = $_POST['deseado'];
+
+    if(isset($num)){
+        if(!isset($oportunidades)){
+            $oportunidades = 4;
+        }
+            if($oportunidades < 4){
+                if($num == $deseado){
+                    echo('Enhorabuena fiera, has encontrado el número, enga a dormir');
+                }else{
+                    $oportunidades--;
+                    echo('Has fallado payaso<br><br>');
+                    echo("Te quedan $oportunidades oportunidades<br><br>");
+                }
+            }else{
+                echo ('Te has quedado sin oportunidades <br><br>');
+            }
+    }else{
+        echo 'No has introducido ningún dato <br><br>';
+    }*/
+
     ?>
     
-    
+    <!--Te quedan <-?php $oportunidades-- ?> oportunidades <br><br> -->
     Introduce un número de 4 cifras.<br>
 
-    <form action="#" method="post">
+    <form action="index.php" method="post">
     
         Número: <input type="number" name="num"><br><br>
         <input type="hidden" name="deseado" value="5810">
