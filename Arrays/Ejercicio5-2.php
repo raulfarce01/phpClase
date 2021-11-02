4db9a90c8b14290bb6da63c373362c360143bb79
Neiroh <raulito72001@gmail.com>
2021-11-02 08:43:14 +0100

    Arrays de polla

--- Arrays/Ejercicio5-2.php
+++ Arrays/Ejercicio5-2.php
@@ -25,48 +25,63 @@
 
         }
 
-        if($countNum == 9){
+        if($countNum == 10){
 
-            //$auxNum = $auxNum , " " , $num;
-
+            $auxNum = $auxNum . " " . $num;
+            $auxNum = substr($auxNum, 2);
             $num = explode(" ", $auxNum);
+
             foreach ($num as $valor){
 
                 if($valor < $min){
 
                     $min = $valor;
 
-                }else{
+                }else if ($valor > $max){
 
                     $max = $valor;
 
                 }
             }
 
-            $auxNum["Máximo"] = $max;
-            $auxNum["Mínimo"] = $min;
-
             foreach ($auxNum as $valor){
 
-                echo $valor , '<br>';
+                if($valor == $min){
+
+                    echo 'Mínimo =>' . $valor . '<br>';
+
+                }else if($valor == $max){
+
+                    echo 'Máximo =>' . $valor . '<br>';
+
+                }else{
+
+                    echo $valor . '<br>';
+
+                }
 
             }
 
         }
         
-    
+    if (($countNum < 10) || (!isset($num))){
     ?>
     
     <form action="#" method="post">
 
         <label for="num">Número:</label>
-        <input type="number" name="num"><br>
-        <input type="hidden" name="countNum" value="<?php $countNum++ ?>">
-        <input type="hidden" name="auxNum" value="<?= $auxNum , " " , $num ?>">
+        <input type="number" name="num" autofocus><br>
+        <input type="hidden" name="countNum" value="<?php ++$countNum ?>">
+        <input type="hidden" name="auxNum" value="<?php $auxNum . " " . $num ?>">
 
         <input type="submit" value="Enviar">
 
     </form>
+    <?php
+
+    }
+
+    ?>
 
 </body>
 </html>
