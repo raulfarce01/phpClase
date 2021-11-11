8c513f3bb6fd3e1feb7a04927048ee6cd385c403
Neiroh <raulito72001@gmail.com>
2021-11-11 20:48:55 +0100

    Validación PHP

--- Prueba/holaMundo.php
+++ Prueba/holaMundo.php
@@ -10,18 +10,104 @@
     
     <?php
 
-        include_once 'funcionFecha.inc.php';
+        //
+        //En esta parte se encarga de comprobar que no estén vacíos los valores del nombre y de los módulos
+        //Y si ha recibido algún valor lo asigna a la variable
+        //
+        if (!empty($_POST['modulos']) && !empty($_POST['nombre'])){
+            $nombre = $_POST['nombre'];
+            $modulos = $_POST['modulos'];
 
-        echo '<table>';
+            //
+            //Aquí muestra el valor del nombre
+            //
+            print "Nombre: " . $nombre . "<br>";
 
-        foreach($_SERVER as $valor1 => $valor2){
+            //
+            //En este foreach se encarga de recorrer el array de módulos para mostrarlo por pantalla
+            //
+            foreach($modulos as $modulo){
 
-            echo '<tr><td>' . $valor1 . '</td><td>' . $valor2 . '</td></tr>';
+                print "Módulo: " . $modulo . "<br>";
+
+            }
+        }else{
+
+        ?>
+
+            <form name="input" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+            Nombre del alumno: <input type="text" name="nombre">
+
+        <?php
+
+            //
+            //En este if comprobamos que no se haya pulsado el botón enviar con el nombre vacío
+            //
+            if(isset($_POST['enviar']) && empty($_POST['nombre'])){
+
+                echo '<span style="color: red;"> &lt;-- Debe introducir un nombre!!</span>';
+
+            }
+
+        ?>
+        <p>
+            Módulos que cursa:
+        </p>
+
+        <?php
+
+            //
+            //Aquí comprobamos que no se pulse el botón enviar sin haber seleccionado al menos un módulo
+            //
+            if(isset($_POST['enviar']) && empty($_POST['modulos'])){
+
+                echo '<span style="color: red;"> &lt;-- Debe escoger al menos uno!!</span>';
+
+            }
+
+        ?>
+
+        </p>
+
+        <input type="checkbox" name="modulos[]" value="DWES"
+        <?php 
+
+            //
+            //Se inicializa como seleccionado el valor del array DWES, por eso está dentro del input esta parte
+            //del código
+            //
+            if(isset($_POST['modulos']) && in_array("DWES", $_POST['modulos'])){
+
+                echo 'checked="checked"';
+
+            }
+
+        ?>>
+        Desarrollo Web Entorno Servidor<br>
+
+        <input type="checkbox" name="modulos[]" value="DWEC"
+        <?php 
+
+            //
+            //Se inicializa como seleccionado el valor del array DWEC, por eso está dentro del input esta parte
+            //del código
+            //
+            if(isset($_POST['modulos']) && in_array("DWEC", $_POST['modulos'])){
+
+                echo 'checked="checked"';
+
+            }
+
+        ?>>
+        Desarrollo Web Entorno Cliente<br><br>
+
+        <input type="submit" value="Enviar" name="enviar">
+
+        <?php
+        
 
         }
 
-        echo '</table>';
-
-        ?>
+    ?>
 </body>
 </html>
