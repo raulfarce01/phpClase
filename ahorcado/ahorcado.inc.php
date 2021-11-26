6d5e412a6233901d946d3dd9f41cbc2a3974c23f
Neiroh <raulito72001@gmail.com>
2021-11-26 10:19:10 +0100

    Ahorcado

---
+++ ahorcado/ahorcado.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+    function dibuja($oportunidad){
+
+        return "<img src='./img/ahorcado$oportunidad.jpg'>";
+
+    }
+
+?>
---
+++ ahorcado/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ahorcado</title>
+</head>
+<body>
+
+    <?php
+
+        include_once 'ahorcado.inc.php';
+
+        $palabras = ["Rinoceronte", "Electricidad", "Antibiotico", "coyuntura", "Cuadriplejia", "Excremento", "Hemiplejia", "Diagnostico"];
+
+        if(!isset($_POST['letra']) || !isset($_POST['oportunidad'])){
+
+            $letra = "";
+            $oportunidad = 6;
+
+            $respuesta = $palabras[rand(0, 7)];
+            $misterio = "";
+
+            $fallo = false;
+
+            //echo $respuesta;
+
+            for($i = 0; $i < strlen($respuesta); $i++){
+
+                $misterio[$i] = "_";
+
+            }
+
+        }else{
+
+                $oportunidad = $_POST['oportunidad'];
+                $misterio = $_POST['misterio'];
+                $letra = $_POST['letra'];
+                $respuesta = $_POST['respuesta'];
+
+                //echo strlen($respuesta);
+
+                //echo $oportunidad;
+
+                //echo $misterio;
+                //echo $respuesta;
+                //print_r($palabras);
+
+                if($oportunidad > 0){
+
+                    if(strlen($letra) > 1){
+
+                        if(strtoupper($letra) == strtoupper($respuesta)){
+
+                            echo 'Enhorabuena, has ganado';
+
+                        }
+
+                    }else{
+
+                        for($i = 0; $i < strlen($respuesta); $i++){
+
+                            //echo "Entra for";
+        
+                            //echo $respuesta[$i] . "<br>";
+        
+                            if(strtoupper($letra) == strtoupper($respuesta[$i])){
+        
+                                $misterio[$i] = strtoupper($letra);
+                                //echo $letra;
+        
+                            }
+        
+                            if(!in_array(strtoupper($letra), strtoupper($respuesta))){
+        
+                                $dibujo = dibuja($oportunidad);
+        
+                            }
+                            
+                            
+                        }
+
+                        $oportunidad--;
+
+                        echo $dibujo;
+
+                        echo "<p> Te quedan " . $oportunidad . " oportunidades </p>";
+                        
+                        for($i = 0; $i < strlen($respuesta); $i++){
+
+                            echo $misterio[$i] . "&nbsp;";
+
+                        }
+                    }
+
+                
+            }else{
+
+                echo "Has perdido todas las oportunidades";
+
+            }
+
+        }
+
+    ?>
+    
+    <form action="#" method="post">
+
+    <p>
+        <label for="letra">Letra/Palabra</label>
+        <input type="text" name="letra" autofocus>
+        <input type="hidden" name="misterio" value="<?php echo $misterio ?>">
+        <input type="hidden" name="oportunidad" value="<?php echo $oportunidad ?>">
+        <input type="hidden" name="respuesta" value="<?php echo $respuesta ?>">
+    </p>
+    <p>
+        <input type="submit" value="Enviar">
+    </p>
+    </form>
+
+</body>
+</html>
