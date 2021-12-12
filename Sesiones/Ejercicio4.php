5e87cac589bad22adf54cbed1a54e41796b3c889
Neiroh <raulito72001@gmail.com>
2021-12-12 02:30:57 +0100

    Sesiones Fin (Algunos Problemillas)

--- Sesiones/Ejercicio4.php
+++ Sesiones/Ejercicio4.php
@@ -16,17 +16,121 @@
 
         const user = array('usuario' => 'Neiroh', 'password' => '123456');
 
-        if(session_id() == ""){
-            session_start();
-        }
-
-        if(!isset($userEntra) && !isset($passEntra)){
+        if(!isset($_POST['userEntra']) && !isset($_POST['passEntra'])){
 
             echo '
             
+                <h2>Inicio de Sesión</h2>
+
+                <form action="#" method="post">
                 
+                    <p>
+                        <label for="user">Usuario: </label>
+                        <input type="text" name="userEntra" placeholder="Ejemplo00" autofocus>
+                    </p>
+                    <p>
+                        <label for="passwd">Contraseña: </label>
+                        <input type="password" name="passEntra">
+                    </p>
+                    <p>
+                        <input type="submit" name="enviar" value="Enviar">
+                    </p>
+                
+                </form>
 
             ';
+
+        }else{
+
+            $userEntra = $_POST['userEntra'];
+            $passEntra = $_POST['passEntra'];
+
+            if($userEntra == user['usuario']){
+
+                if($passEntra == user['password']){
+
+                    echo '
+                    
+                        Iniciando sesión...<br>
+                        Sesión Iniciada.<br>
+                        Saludos, ' . $userEntra . '<br>
+
+                        <form action="#" method="post">
+                            <input type="submit" name="logout" value="Cerrar Sesión">
+                        </form>
+                    
+                    ';
+
+                    if(session_id() == ""){
+                        session_start();
+                    }
+
+                    if($_POST['logout']){
+
+                        unset($userEntra);
+                        unset($passEntra);
+
+                        session_destroy();
+
+                        echo 'Cerrando Sesión...<br>';
+                        echo 'Sesión Cerrada<br>';
+
+                    }
+
+                }else{
+
+                    echo '
+                    
+                        <h2>Inicio de Sesión</h2>
+
+                        <form action="#" method="post">
+                        
+                            <p>
+                                <label for="user">Usuario: </label>
+                                <input type="text" name="userEntra" placeholder="Ejemplo00" value="' . $userEntra . '">
+                            </p>
+                            <p>
+                                <label for="passwd">Contraseña: </label>
+                                <input type="password" name="passEntra" autofocus>
+                                <span style="color: red;">Contraseña Incorrecta...</span>
+                            </p>
+                            <p>
+                                <input type="submit" name="enviar" value="Enviar">
+                            </p>
+                        
+                        </form>
+                    
+                    ';
+
+                }
+
+            }else{
+
+                echo '
+                    
+                        <h2>Inicio de Sesión</h2>
+
+                        <form action="#" method="post">
+                        
+                            <p>
+                                <label for="user">Usuario: </label>
+                                <input type="text" name="userEntra" placeholder="Ejemplo00" value="' . $userEntra . '"autofocus>
+                                <span style="color: red;">Usuario Incorrecto</span>
+                            </p>
+                            
+                            <p>
+                                <label for="passwd">Contraseña: </label>
+                                <input type="password" name="passEntra">
+                            </p>
+                            <p>
+                                <input type="submit" name="enviar" value="Enviar">
+                            </p>
+                        
+                        </form>
+                    
+                    ';
+
+            }
 
         }
 
